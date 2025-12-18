7ecb7cd342821ce78c86de002ee48799165e1631
Dan Carley <dan.carley@gmail.com>
2025-12-18 08:01:57 +0000

    sync: Pairss typo.

--- src/sync/engine.rs
+++ src/sync/engine.rs
@@ -131,7 +131,7 @@ where
 
         let matches_count = matches.len();
         if matches_count > 0 {
-            info!(count = matches_count, "Transaction pairss matched");
+            info!(count = matches_count, "Transaction pairs matched");
         }
 
         Ok(())
